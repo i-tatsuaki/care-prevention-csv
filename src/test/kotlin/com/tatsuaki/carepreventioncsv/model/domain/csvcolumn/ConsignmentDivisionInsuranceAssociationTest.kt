564c74a0a95735ccaa15211f77aae87592ad9fe5
tatsuaki <k05371@yahoo.co.jp>
2019-12-30 18:52:30 +0900

    test: add new test

--- src/test/kotlin/com/tatsuaki/carepreventioncsv/model/domain/csvcolumn/ConsignmentDivisionInsuranceAssociationTest.kt
+++ src/test/kotlin/com/tatsuaki/carepreventioncsv/model/domain/csvcolumn/ConsignmentDivisionInsuranceAssociationTest.kt
@@ -1,6 +1,7 @@
 package com.tatsuaki.carepreventioncsv.model.domain.csvcolumn
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -13,11 +14,18 @@ class ConsignmentDivisionInsuranceAssociationCodeTest {
                 arguments("1", true),
                 arguments("0", false)
         )
+
+        @JvmStatic
+        fun parameters_getJapanese() = listOf(
+                arguments("1", "委託する"),
+                arguments("2", "委託しない")
+        )
     }
 
     @ParameterizedTest
     @MethodSource("parameters_contains")
     fun contains (
+
             code: String,
             isExistsCode: Boolean
     ) {
@@ -26,5 +34,18 @@ class ConsignmentDivisionInsuranceAssociationCodeTest {
 
         // then
         assertThat(actual).isEqualTo(isExistsCode)
+    }
+
+    @ParameterizedTest
+    @MethodSource("parameters_getJapanese")
+    fun getJapanese (
+            code: String,
+            japanese: String
+    )  {
+        // when
+        val actual = ConsignmentDivisionInsuranceAssociation.Code.getJapanese(code)
+
+        // then
+        assertThat(actual).isEqualTo(japanese)
     }
 }
